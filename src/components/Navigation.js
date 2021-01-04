32d60c76744186ae8ecc4a47c94a8322b93cf53c
Samuel Davis <samueljakdavis@gmail.com>
2021-01-03 20:05:14 -0500

    make nav more visible, let click passthrough

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,12 @@ import styles from "./Navigation.module.css";
 function Navigation({ onNavigate }) {
   return (
     <nav className={styles.Nav}>
-      <a
-        href="/"
-        onClick={onNavigate.bind(null, -1)}
-        title="previous"
-        dangerouslySetInnerHTML={{ __html: "&laquo;" }}
-      />
-      <a
-        href="/"
-        onClick={onNavigate.bind(null, +1)}
-        title="next"
-        dangerouslySetInnerHTML={{ __html: "&raquo;" }}
-      />
+      <a href="/" onClick={onNavigate.bind(null, -1)} title="previous">
+        Back
+      </a>
+      <a href="/" onClick={onNavigate.bind(null, +1)} title="next">
+        Next
+      </a>
     </nav>
   );
 }
